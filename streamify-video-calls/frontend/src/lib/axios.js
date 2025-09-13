4431d46db2e1d5718463244ef32641821a48e864
roshanimaurya.in@gmail.com <avasheshmaurya419@gmail.com>
2025-09-13 14:13:37 +0530

    host

--- streamify-video-calls/frontend/src/lib/axios.js
+++ streamify-video-calls/frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
+    ? "https://team-video-calling-chat.vercel.app/api"
     : "/api";
 
 export const axiosInstance = axios.create({
